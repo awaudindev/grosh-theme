55f037627f1abdab7ef2daa96d0bcc4b0be87a27
wedangsusu <aura.anar@gmail.com>
2017-05-23 06:30:24 +0800

    update button style

--- woocommerce/myaccount/form-edit-address.php
+++ woocommerce/myaccount/form-edit-address.php
@@ -42,8 +42,8 @@ do_action( 'woocommerce_before_edit_account_address_form' ); ?>
 
 		<?php do_action( "woocommerce_after_edit_address_form_{$load_address}" ); ?>
 
-		<p>
-			<input type="submit" class="button" name="save_address" value="<?php esc_attr_e( 'Save Address', 'woocommerce' ); ?>" />
+		<p style="float:right; margin-top:5px;">
+			<input type="submit" class="btn btn-default" name="save_address" value="<?php esc_attr_e( 'Save Address', 'woocommerce' ); ?>" />
 			<?php wp_nonce_field( 'woocommerce-edit_address' ); ?>
 			<input type="hidden" name="action" value="edit_address" />
 		</p>
