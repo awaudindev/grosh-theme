762df630206de819c67c515f6dedf56a64475e10
wedangsusu <aura.anar@gmail.com>
2017-06-28 11:48:20 +0800

    try search

--- inc/woo-inc.php
+++ inc/woo-inc.php
@@ -266,4 +266,53 @@ function save_extra_fields($order_id, $posted) {
 	}
 }
 
+add_action('pre_get_posts', 'jc_woo_search_pre_get_posts');
+function jc_woo_search_pre_get_posts($q){
+ 
+    if ( is_search() ) {
+        add_filter( 'posts_join', 'jc_search_post_join' );
+        add_filter( 'posts_where', 'jc_search_post_excerpt' );
+    }
+}
+
+/**
+ * Add Custom Join Code for wp_mostmeta table
+ * @param  string $join
+ * @return string
+ */
+function jc_search_post_join($join = ''){
+ 
+    global $wp_the_query;
+ 
+    // escape if not woocommerce searcg query
+    if ( empty( $wp_the_query->query_vars['wc_query'] ) || empty( $wp_the_query->query_vars['s'] ) )
+            return $join;
+ 
+    $join .= "INNER JOIN wp_postmeta AS jcmt1 ON (wp_posts.ID = jcmt1.post_id)";
+    return $join;
+}
+
+/**
+ * Add custom where statement to product search query
+ * @param  string $where
+ * @return string
+ */
+function jc_search_post_excerpt($where = ''){
+ 
+    global $wp_the_query;
+ 
+    // escape if not woocommerce search query
+    if ( empty( $wp_the_query->query_vars['wc_query'] ) || empty( $wp_the_query->query_vars['s'] ) )
+            return $where;
+ 
+    $where = preg_replace("/post_title LIKE ('%[^%]+%')/", "post_title LIKE $1)
+    			OR (post_content LIKE $1)
+                OR (jcmt1.meta_key = '_sku' AND CAST(jcmt1.meta_value AS CHAR) LIKE $1)
+                OR  (jcmt1.meta_key = '_author' AND CAST(jcmt1.meta_value AS CHAR) LIKE $1)
+                OR  (jcmt1.meta_key = '_publisher' AND CAST(jcmt1.meta_value AS CHAR) LIKE $1)
+                OR  (jcmt1.meta_key = '_format' AND CAST(jcmt1.meta_value AS CHAR) LIKE $1 ", $where);
+ 
+    return $where;
+}
+
 ?>
