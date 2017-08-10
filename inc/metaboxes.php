560baf75908f88a1c5976d95b79ef3c6b24b6226
awaludin <awaludinakbar@yahoo.com>
2017-08-10 23:41:58 +0700

    update

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -15,7 +15,17 @@ function grosh_metaboxes( array $meta_boxes ) {
 
 	// Start with an underscore to hide fields from custom fields list
 	$prefix = '_grosh_';
+
+	$fontData = googleFonts();
+
+	$listFonts = array();
 	
+	if($fontData){
+		foreach ($fontData as $each) {
+			array_push($listFonts, array('value' => $each['family'],'name'=>$each['family']));
+		}
+	}
+
 	$meta_boxes[] = array(
 		'id'         => 'general_settings',
 		'title'      => 'Product General settings',
@@ -28,8 +38,16 @@ function grosh_metaboxes( array $meta_boxes ) {
                 'name' => 'Font Size',
                 'desc' => 'Set a font size.',
                 'id'   => $prefix . 'font_size',
-                'type' => 'text',
+                'type' => 'text_small',
             ),
+			array(
+				'name' => 'Google Fonts',
+				'desc' => 'Choose google font',
+				'id'   => $prefix . 'google_font',
+				'type' => 'select',
+				'options' => $listFonts,
+				'value' => ''
+			),
 		)
 	);
 
@@ -48,4 +66,62 @@ function initialize_meta_boxes() {
     if ( ! class_exists( 'kleo_Meta_Box' ) ) {
     	require_once get_template_directory() . '/inc/metaboxes/init.php';
     }
+}
+
+function googleFonts(){
+
+	global $wpdb;
+		$table_name = $wpdb->prefix.'googlefonts';
+		if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {	
+		     //table not in database. Create new table
+		     $charset_collate = $wpdb->get_charset_collate();
+		 
+		     $sql = "CREATE TABLE $table_name (
+		          id mediumint(9) NOT NULL AUTO_INCREMENT,
+		          family text NOT NULL,
+		          category text NOT NULL,
+		          UNIQUE KEY id (id)
+		     ) $charset_collate;";
+		     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		     dbDelta( $sql );
+
+		}else{
+			$result = $wpdb->get_results("SELECT * FROM $table_name");
+			$fontsList = array();
+
+			if(count($result) < 1){
+				$fontUrl = file_get_contents('https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyBwIX97bVWr3-6AIUvGkcNnmFgirefZ6Sw');
+				$fontData = get_object_vars(json_decode($fontUrl));
+
+				foreach ($fontData['items'] as $each) {
+					$wpdb->insert($table_name,array(
+						'family' => $each->family,
+						'category' => $each->category
+					));
+					
+					array_push($fontsList,array('family'=>$each->family));
+				}
+			}else{
+				foreach ($result as $each) {
+					array_push($fontsList,array('family'=>$each->family));
+				}
+				sort($fontsList);
+			}
+		}
+
+	return $fontsList;
+}
+
+function applyFonts($postid){
+
+	$size = get_post_meta($postid,'_grosh_font_size');
+	$font = get_post_meta($postid,'_grosh_google_font');
+
+	$result = '<style type="text/css">
+				@import url("https://fonts.googleapis.com/css?family=Roboto");
+				font-family: "Roboto", sans-serif;
+				</style>';
+
+	return $result;
+
 }
